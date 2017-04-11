7e368eccfd5e0d297a9a6438bc563569fd16c50d
gothic17 <gothic1711@op.pl>
2017-04-11 23:09:00 +0200

    Zadanie 36 i 37 z listy 4 - Big Data

--- BigData/Lista 4/Zadanie 36/Jaccard.scala
+++ BigData/Lista 4/Zadanie 36/Jaccard.scala
@@ -2,48 +2,19 @@ import scala.io.Source
 import java.io._
 import scala.collection.mutable
 
-def jaccard(f1:String, f2:String, k:Integer) :Double = {
-  var book1 = Source.fromFile(f1, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase //.split("\\s+")
-  var book2 = Source.fromFile(f2, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase //.split("\\s+")
-  val list1 = mutable.Set.empty[String]
-  val list2 = mutable.Set.empty[String]
+object Jaccard {
 
-  //book1.iterator.sliding(k).withPartial(false).map(x=>x.mkString).toSet
+	def jaccard(f1:String, f2:String, k:Integer) :Double = {
+		var book1 = Source.fromFile(f1, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase
+		var book2 = Source.fromFile(f2, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase
+		var kGrams1 = book1.iterator.sliding(k).withPartial(false).map(x=>x.mkString).toSet
+		var kGrams2 = book2.iterator.sliding(k).withPartial(false).map(x=>x.mkString).toSet
 
-  for (i <- Range(0, book1.length())) {
-    if (i + k < book1.length()) {
-      list1 += book1.substring(i, i + k)
-    }
-    //println(Book.substring(i, i+3))
-  }
-  for (i <- Range(0, book2.length())) {
-    if (i + k < book2.length()) {
-      list1 += book2.substring(i, i + k)
-    }
-    //println(Book.substring(i, i+3))
-  }
-  //println((list1 & list2).size().toDouble/((list1 | list2).size().toDouble()))
-  println("123")
-  println((list1 & list2).size.toDouble)
-  println("321321")
-  return 0.0
+		return (kGrams1 & kGrams2).size.toDouble/((kGrams1 | kGrams2).size.toDouble)
+	}
+
+	def main(args: Array[String]): Unit = {
+		println(jaccard(args(0), args(1), args(2).toInt))
+	}
 }
 
-def main(args: Array[String]): Unit = {
-  println("dwa")
-  jaccard("Ostatnie_zyczenie.txt", "Ostatnie_zyczenie.txt", 3)
-  println("dwafe")
-}
-/*
-var Groupped = Filtered.groupBy(x=>x)
-
-var Reduced = Groupped.mapValues(x=>x.length)
-
-var Final = Reduced.toSeq.sortWith((x,y)=>x._2>y._2)
-
-val writer = new PrintWriter(new File("List.txt" ))
-
-for (pair <- Final) {
-  writer.write(pair._2 + " " + pair._1 + "\n")
-}
-writer.close()*/
--- BigData/Lista 4/Zadanie 37/MinHash.scala
+++ BigData/Lista 4/Zadanie 37/MinHash.scala
@@ -16,27 +16,36 @@ object MinHash {
 
 //-----------------------------------------------------------------
 
-	def minHash(f1:String, f2:String, k:Integer, h:Integer) :Unit = {
+	def minHash(f1:String, f2:String, k:Integer, h:Integer) :Double = {
 		var book1 = Source.fromFile(f1, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase
 		var book2 = Source.fromFile(f2, "UTF-8").mkString.replaceAll("""[\p{Punct}]""", "").replaceAll("—", "").replaceAll(" ", "").toLowerCase
 		var kGrams1 = book1.iterator.sliding(k).withPartial(false).map(x=>x.mkString).toSet
 		var kGrams2 = book2.iterator.sliding(k).withPartial(false).map(x=>x.mkString).toSet
 
-		var hashes1 = mutable.Set.empty[Int]
+		var hashes1_all = mutable.Set.empty[Int]
+		var hashes1_min = mutable.Set.empty[Int]
+		var hashes2_all = mutable.Set.empty[Int]
+		var hashes2_min = mutable.Set.empty[Int]
 
 		var seed = 1234567890
-		for (i <- Range(0, h)) {
+		for (i <- Range(1, h + 1)) {
+			hashes1_all = mutable.Set.empty[Int]
+			hashes2_all = mutable.Set.empty[Int]
 			for (k <- kGrams1) {
-				hashes1 += MurmurHash3.stringHash(k, seed + i)
+				hashes1_all += MurmurHash3.stringHash(k, seed + i)
 			}
+			hashes1_min += hashes1_all.min
+			for (k <- kGrams2) {
+				hashes2_all += MurmurHash3.stringHash(k, seed + i)
+			}
+			hashes2_min += hashes2_all.min
 		}
-
-		println(hashes1.min)
+		return (hashes1_min & hashes2_min).size.toDouble / hashes1_min.size.toDouble
 
 	}
 
 	def main(args: Array[String]): Unit = {
-		//println(jaccard(args(0), args(1), args(2).toInt))
-		minHash(args(0), args(1), args(2).toInt, args(3).toInt)
+		println("Dokładna wartość: " + jaccard(args(0), args(1), args(2).toInt))
+		println("MinHash: " + minHash(args(0), args(1), args(2).toInt, args(3).toInt))
 	}
 }
