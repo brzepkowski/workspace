f7a556fa6dc9d0eb9e888e5af55d4b5734f5d36b
gothic17 <gothic1711@op.pl>
2017-05-16 21:38:33 +0200

    Skończony Merkle-Hellman, został Shamir's attack

--- Kryptografia II/Lista_4/Zad_1.jl
+++ Kryptografia II/Lista_4/Zad_1.jl
@@ -19,6 +19,7 @@ function genKey(n)
   while (gcd(r, q) != 1)
     r = rand(1:q)
   end
+  println("q = ", q, ", r = ", r, ", gcd = ", gcd(r,q))
 
   β = zeros(BigInt, n)
   for i in 1:n
@@ -32,14 +33,6 @@ genKey(20)
 
 function encrypt(pathToFile::String)
   (β, w, q, r) = genKey(8)
-  β = BigInt[479,972,930,888,283,559,611,743]
-  w = BigInt[6,10,21,41,83,162,325,653]
-  q = 1303
-  r = 186
-  println("β = ", β)
-  println("w = ", w)
-  println("q = ", q)
-  println("r = ", r)
   readFile = open(pathToFile);
   s = readstring(readFile)
 
@@ -48,14 +41,13 @@ function encrypt(pathToFile::String)
   for plaintext in s # Iterate over each char in String
     ciphertext = 0
     α = bits(plaintext)
-    α = [α[i] for i in 24:32]
-    print("bits = ", α, ", | ")
+    α = [α[i] for i in 25:32]
     for j in 1:8 # j represents each bit of char i
-      #ciphertext += j * β
-      ciphertext += BigInt(α[j]) * β[j]
+      if α[j] == '1'
+        ciphertext += β[length(β) - j + 1]
+      end
     end
     write(writeFile, string(ciphertext,"\n"))
-    println(plaintext, " => ", ciphertext)
   end
   close(readFile)
   close(writeFile)
@@ -64,36 +56,20 @@ end
 
 function decrypt(pathToFile::String, w, q, r)
   readFile = open(pathToFile)
-
-  w = BigInt[6,10,21,41,83,162,325,653]
-  q = 1303
-  r = 186
-  println("w = ", w)
-  println("q = ", q)
-  println("r = ", r)
-
   s = invmod(r, q) # Obliczamy s tak, aby spełniało sr = 1 (mod q)
-  println("s = ", s)
 
   for c in eachline(readFile)
     cˈ = (parse(BigInt, c)*s) % q
-    # println("c = ", c, ", s = ", s, ", c' = ", cˈ)
     # cˈ = cs = ∑αᵢβᵢs (mod q), a ponieważ βᵢ = rwᵢ otrzymujemy
     # βᵢs = wᵢrs = wᵢ (mod q), więc
     # cˈ = ∑ αᵢwᵢ (mod q)
-    println("c = ", c, " , c' = ", cˈ)
-    plaintext = Int(0)
+    plaintext = Char(0)
     for i in length(w):-1:1 # Jeśli ostatni element wₖ > cˈ, to αₖ = 0, a jeśli wₖ ≦ cˈ, to αₖ = 1
-      αᵢ = 0
       if w[i] <= cˈ
-        αᵢ = 1
+        plaintext += (2^(i-1))
+        cˈ = cˈ - w[i]
       end
-      cˈ = cˈ - w[i]*αᵢ
-      print(αᵢ, ", ")
-      plaintext += (2^(i-1)) * αᵢ
-      # plaintext = string(αᵢ, plaintext)
     end
-    # plaintext = string(plaintext)
     println(" -> ", plaintext)
   end
 end
