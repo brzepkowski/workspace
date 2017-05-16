982e1e42fc595dbbf94fd20e188a3dd26ea34d34
gothic17 <gothic1711@op.pl>
2017-05-16 12:31:02 +0200

    Praca nad listą 4 z kryptografii - Merkle-Hellman

--- Kryptografia II/Lista_4/Zad_1.jl
+++ Kryptografia II/Lista_4/Zad_1.jl
@@ -31,7 +31,15 @@ end
 genKey(20)
 
 function encrypt(pathToFile::String)
-  (β, w, q, r) = genKey(32)
+  (β, w, q, r) = genKey(8)
+  β = BigInt[479,972,930,888,283,559,611,743]
+  w = BigInt[6,10,21,41,83,162,325,653]
+  q = 1303
+  r = 186
+  println("β = ", β)
+  println("w = ", w)
+  println("q = ", q)
+  println("r = ", r)
   readFile = open(pathToFile);
   s = readstring(readFile)
 
@@ -40,7 +48,9 @@ function encrypt(pathToFile::String)
   for plaintext in s # Iterate over each char in String
     ciphertext = 0
     α = bits(plaintext)
-    for j in 1:32 # j represents each bit of char i
+    α = [α[i] for i in 24:32]
+    print("bits = ", α, ", | ")
+    for j in 1:8 # j represents each bit of char i
       #ciphertext += j * β
       ciphertext += BigInt(α[j]) * β[j]
     end
@@ -52,9 +62,41 @@ function encrypt(pathToFile::String)
   return (β, w, q, r)
 end
 
-function decrypt(pathToFile::String, β, w, q, r)
-  s = invmod(r, q)
+function decrypt(pathToFile::String, w, q, r)
+  readFile = open(pathToFile)
+
+  w = BigInt[6,10,21,41,83,162,325,653]
+  q = 1303
+  r = 186
+  println("w = ", w)
+  println("q = ", q)
+  println("r = ", r)
+
+  s = invmod(r, q) # Obliczamy s tak, aby spełniało sr = 1 (mod q)
+  println("s = ", s)
+
+  for c in eachline(readFile)
+    cˈ = (parse(BigInt, c)*s) % q
+    # println("c = ", c, ", s = ", s, ", c' = ", cˈ)
+    # cˈ = cs = ∑αᵢβᵢs (mod q), a ponieważ βᵢ = rwᵢ otrzymujemy
+    # βᵢs = wᵢrs = wᵢ (mod q), więc
+    # cˈ = ∑ αᵢwᵢ (mod q)
+    println("c = ", c, " , c' = ", cˈ)
+    plaintext = Int(0)
+    for i in length(w):-1:1 # Jeśli ostatni element wₖ > cˈ, to αₖ = 0, a jeśli wₖ ≦ cˈ, to αₖ = 1
+      αᵢ = 0
+      if w[i] <= cˈ
+        αᵢ = 1
+      end
+      cˈ = cˈ - w[i]*αᵢ
+      print(αᵢ, ", ")
+      plaintext += (2^(i-1)) * αᵢ
+      # plaintext = string(αᵢ, plaintext)
+    end
+    # plaintext = string(plaintext)
+    println(" -> ", plaintext)
+  end
 end
 
 (β, w, q, r) = encrypt("input")
-decrypt("input_enc", β, w, q, r)
+decrypt("input_enc", w, q, r)
