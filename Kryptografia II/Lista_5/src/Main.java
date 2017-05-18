ee32930d804c5687bfb69632687d3f7f218da7ec
gothic17 <gothic1711@op.pl>
2017-05-18 12:23:19 +0200

    Poczatek implementacji systemu Rabina z listy 5 na kryptografię

---
+++ Kryptografia II/Lista_5/src/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Main {
+
+    public static BigInteger[] generateKeys() {
+        Random randomGenerator = new Random();
+        BigInteger p = new BigInteger(256, randomGenerator);
+        BigInteger q = new BigInteger(256, randomGenerator);
+        while (!p.isProbablePrime(1000000)) {
+            p = new BigInteger(256, randomGenerator);
+        }
+        while (!q.isProbablePrime(1000000)) {
+            q = new BigInteger(256, randomGenerator);
+        }
+        BigInteger n = p.multiply(q);
+
+        BigInteger[] keys = {p, q, n}; // p and q are private keys / n is public key
+        return keys;
+    }
+
+    public static void main(String[] args) {
+        String mode = args[0];
+        BigInteger[] keys = generateKeys();
+        BigInteger p = keys[0];
+        BigInteger q = keys[1];
+        BigInteger n = keys[2];
+
+        if (mode.equals("encrypt")) {
+            System.out.println("Szyfrowanie...");
+            try {
+                BufferedReader readFileBR = new BufferedReader(new FileReader(args[1]));
+                PrintWriter writeFilePW = new PrintWriter(args[2]);
+                int currentSign = -1;
+                while ((currentSign = readFileBR.read()) != -1) {
+                    BigInteger m = BigInteger.valueOf(currentSign);
+                    BigInteger c = m.pow(2).mod(n); // c = m^2 (mod n)
+                    writeFilePW.println(c.toString());
+                }
+                readFileBR.close();
+                writeFilePW.close();
+            } catch (Exception e) {
+                System.out.println("Błąd: " + e);
+            }
+
+        } else if (mode.equals("decrypt")) {
+            System.out.println("Deszyfrowanie");
+        } else {
+            System.out.println("Podano niepoprawny tryb działania: " + mode);
+        }
+    }
+}
