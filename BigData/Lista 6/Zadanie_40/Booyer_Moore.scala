7b450e457711c584dae509b6e56e9bffb3871a64
gothic17 <gothic1711@op.pl>
2017-06-18 18:25:11 +0200

    Praca nad projektem z kryptografii

--- BigData/Lista 6/Zadanie_40/Booyer_Moore.scala
+++
@@ -1,7 +0,0 @@
-def Booyer_Moore(list:List[String]) = {
-  var c = 0
-  var m = null
-
-
-
-}
--- Kryptografia II/Projekt/RandomWalker.jl
+++ Kryptografia II/Projekt/RandomWalker.jl
@@ -2,11 +2,28 @@ using LightGraphs
 
 
 function randomWalker(N, d, l)
-  S = collect(1:N)
-  println(S[randperm(length(S))])
-  graph = DiGraph(N)
+  S = [] # Tablica zawierająca wszystkie permutacje Sⱼ
+  indexes = Dict{Int, Int}()
 
-  println(graph)
+  # Zrobione do umożliwienia iterowania od 0
+  for i in 0:N
+    indexes[i] = i+1
+  end
+
+  s = collect(0:N-1)
+  for i in 1:d
+    push!(S, s[randperm(length(s))])
+  end
+
+  vₖ = 0
+  for k in 0:l-1
+    # println(S[indexes[k % d]])
+    # println("k = ", k ,"(", indexes[k], ") , k mod d = ", k % d,"(", indexes[k%d], ") , vₖ = ", vₖ, "(", indexes[vₖ], ") , vₖ+1 = ", S[indexes[k % d]][vₖ + 1], "(", indexes[S[indexes[k % d]][vₖ + 1]] , ")")
+    vₖ = S[indexes[k % d]][vₖ + 1]
+  end
+  println(vₖ)
 end
 
-randomWalker(10, 2, 2)
+for i in 1:100
+  randomWalker(10, 2, 6)
+end
