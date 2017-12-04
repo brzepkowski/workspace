f3f53bd6027fd616d25c39fbcf6a274100f7fcd0
gothic17 <gothic1711@op.pl>
2017-12-04 12:42:57 +0100

    Lisa 2 z Metod Probabilistycznych Algorytmiki

--- Metody Probabilistyczne Algorytmiki/Lista_1/QuickSort.jl
+++ Metody Probabilistyczne Algorytmiki/Lista_1/QuickSort.jl
@@ -44,9 +44,13 @@ function testQuickSort(m, n)
   ECₙ² = 0
   ESₙ = 0
   ESₙ² = 0
+  CₙResults = []
+  SₙResults = []
   for i in 1:m
     A = shuffle(1:n)
     Cₙ, Sₙ = quickSort(A, 1, length(A))
+    push!(CₙResults, Cₙ)
+    push!(SₙResults, Sₙ)
     ECₙ += Cₙ
     ECₙ² += Cₙ^2
     ESₙ += Sₙ
@@ -59,10 +63,11 @@ function testQuickSort(m, n)
   VCₙ = ECₙ² - ECₙ^2
   VSₙ = ESₙ² - ESₙ^2
   println("ECₙ = ", ECₙ, ", ESₙ = ", ESₙ)
-  EC = (2*(n + 1)*Harmonic(n+1)) - ((8/3)*(n+1))
-  ES = ((1/3)*(n+1)*Harmonic(n+1)) - ((7/9)*(n+1)) + (1/2)
-  println("EC  = ", EC, ", ES  = ", ES)
+  # EC = (2*(n + 1)*Harmonic(n+1)) - ((8/3)*(n+1))
+  # ES = ((1/3)*(n+1)*Harmonic(n+1)) - ((7/9)*(n+1)) + (1/2)
+  # println("EC  = ", EC, ", ES  = ", ES)
   println("VCₙ = ", VCₙ, ", VSₙ = ", VSₙ)
+  return CₙResults, SₙResults, ECₙ, VCₙ, ESₙ, VSₙ
 end # testQuickSort
 
 # Yaroslavskiy's version
@@ -72,11 +77,11 @@ function dualPivotQuickSort(A, left, right)
   if right - left >= 1
     p = A[left]; q = A[right]
     if p > q
-      Cₙ = Cₙ + 1
       p = A[right]
       q = A[left]
       A[left], A[right] = A[right], A[left]; Sₙ = Sₙ + 1
     end
+    Cₙ = Cₙ + 1
     l = left + 1; g = right - 1; k = l
     while k <= g
       Cₙ = Cₙ + 1
@@ -140,43 +145,133 @@ function testDualPivotQuickSort(m, n)
   VCₙ = ECₙ² - ECₙ^2
   VSₙ = ESₙ² - ESₙ^2
   println("ECₙ = ", ECₙ, ", ESₙ = ", ESₙ)
-  EC = ((19/10)*(n + 1)*Harmonic(n+1)) - ((711/200)*(n+1)) + (3/2)
-  ES = ((3/5)*(n+1)*Harmonic(n+1)) - ((27/100)*(n+1)) - (7/12)
-  println("EC  = ", EC, ", ES  = ", ES)
+  # EC = ((19/10)*(n + 1)*Harmonic(n+1)) - ((711/200)*(n+1)) + (3/2)
+  # ES = ((3/5)*(n+1)*Harmonic(n+1)) - ((27/100)*(n+1)) - (7/12)
+  # println("EC  = ", EC, ", ES  = ", ES)
   println("VCₙ = ", VCₙ, ", VSₙ = ", VSₙ)
-  return CₙResults, SₙResults, ECₙ, VCₙ
+  return CₙResults, SₙResults, ECₙ, VCₙ, ESₙ, VSₙ
 end # testQuickSort
 
-# testQuickSort(100, 10)
-# testDualPivotQuickSort(100, 10)
-
+# testQuickSort(1000, 100)
+# testDualPivotQuickSort(1000, 100)
 
 # m - liczba eksperymentów, n - długość tablicy
-function plotDualPivotQuickSort(m, n)
+function plotComparisonsQuickSort(m, n, ax)
   x = []
   CₙResults = []
-  SₙResults = []
   ECₙResults = []
   VCₙResults = []
   Bounds1 = []
   Bounds2 = []
-  for i in 1:n
+  for i in 1000:1000:n
     push!(x, i)
-    Cₙ, Sₙ, ECₙ, VCₙ = testDualPivotQuickSort(m, i) # Zamienione oznaczenie Cₙ i Sₙ (w tym miejscu to są tablice)
+    Cₙ, Sₙ, ECₙ, VCₙ, ESₙ, VSₙ = testQuickSort(m, i) # Zamienione oznaczenie Cₙ i Sₙ (w tym miejscu to są tablice)
     push!(CₙResults, Cₙ)
-    push!(SₙResults, Sₙ)
-    # push!(ECₙResults, ECₙ)
+    push!(ECₙResults, ECₙ)
     push!(VCₙResults, VCₙ)
-    # push!(Bounds1, ECₙ + 2*sqrt(VCₙ))
-    # push!(Bounds2, ECₙ - 2*sqrt(VCₙ))
-    EC = ((19/10)*(i + 1)*Harmonic(i+1)) - ((711/200)*(i+1)) + (3/2)
-    push!(ECₙResults, EC)
+    push!(Bounds1, ECₙ + 2*sqrt(VCₙ))
+    push!(Bounds2, ECₙ - 2*sqrt(VCₙ))
+    # EC = ((19/10)*(i + 1)*Harmonic(i+1)) - ((711/200)*(i+1)) + (3/2)
+    # push!(ECₙResults, EC)
   end
-  PyPlot.plot(x, CₙResults, color="red", "o")
-  PyPlot.plot(x, ECₙResults, color="blue", linestyle="-")
-  # PyPlot.plot(x, Bounds1, color="black", linestyle="--")
-  # PyPlot.plot(x, Bounds2, color="black", linestyle="--")
-  title("Wyniki")
+  ax[:plot](x, CₙResults, color="blue", "+")
+  ax[:plot](x, ECₙResults, label="QuickSort", color="blue", linestyle="-")
+  ax[:plot](x, Bounds1, color="black", "--")
+  ax[:plot](x, Bounds2, color="black", "--")
+  ax[:legend](loc="best")
+  grid("on")
+  title("Comparisons")
 end #plotDualPivotQuickSort
 
-plotDualPivotQuickSort(10, 100)
+# m - liczba eksperymentów, n - długość tablicy
+function plotComparisonsDualPivotQuickSort(m, n, ax)
+  x = []
+  CₙResults = []
+  ECₙResults = []
+  VCₙResults = []
+  Bounds1 = []
+  Bounds2 = []
+  for i in 1000:1000:n
+    push!(x, i)
+    Cₙ, Sₙ, ECₙ, VCₙ, ESₙ, VSₙ = testDualPivotQuickSort(m, i) # Zamienione oznaczenie Cₙ i Sₙ (w tym miejscu to są tablice)
+    push!(CₙResults, Cₙ)
+    push!(ECₙResults, ECₙ)
+    push!(VCₙResults, VCₙ)
+    push!(Bounds1, ECₙ + 2*sqrt(VCₙ))
+    push!(Bounds2, ECₙ - 2*sqrt(VCₙ))
+    # EC = ((19/10)*(i + 1)*Harmonic(i+1)) - ((711/200)*(i+1)) + (3/2)
+    # push!(ECₙResults, EC)
+  end
+  ax[:plot](x, CₙResults, color="red", "+")
+  ax[:plot](x, ECₙResults, label="Dual Pivot QuickSort", color="red", linestyle="-")
+  ax[:plot](x, Bounds1, color="black", "--")
+  ax[:plot](x, Bounds2, color="black", "--")
+  ax[:legend](loc="best")
+  grid("on")
+  title("Comparisons")
+end #plotDualPivotQuickSort
+
+
+# m - liczba eksperymentów, n - długość tablicy
+function plotSwapsQuickSort(m, n, ax)
+  x = []
+  SₙResults = []
+  ESₙResults = []
+  VSₙResults = []
+  Bounds1 = []
+  Bounds2 = []
+  for i in 1000:1000:n
+    push!(x, i)
+    Cₙ, Sₙ, ECₙ, VCₙ, ESₙ, VSₙ = testQuickSort(m, i) # Zamienione oznaczenie Cₙ i Sₙ (w tym miejscu to są tablice)
+    push!(SₙResults, Sₙ)
+    push!(ESₙResults, ESₙ)
+    push!(VSₙResults, VSₙ)
+    push!(Bounds1, ESₙ + 2*sqrt(VSₙ))
+    push!(Bounds2, ESₙ - 2*sqrt(VSₙ))
+    # EC = ((19/10)*(i + 1)*Harmonic(i+1)) - ((711/200)*(i+1)) + (3/2)
+    # push!(ECₙResults, EC)
+  end
+  ax[:plot](x, SₙResults, color="blue", "+")
+  ax[:plot](x, ESₙResults, label="QuickSort", color="blue", linestyle="-")
+  ax[:plot](x, Bounds1, color="black", "--")
+  ax[:plot](x, Bounds2, color="black", "--")
+  ax[:legend](loc="best")
+  grid("on")
+  title("Swaps")
+end #plotDualPivotQuickSort
+
+# m - liczba eksperymentów, n - długość tablicy
+function plotSwapsDualPivotQuickSort(m, n, ax)
+  x = []
+  SₙResults = []
+  ESₙResults = []
+  VSₙResults = []
+  Bounds1 = []
+  Bounds2 = []
+  for i in 1000:1000:n
+    push!(x, i)
+    Cₙ, Sₙ, ECₙ, VCₙ, ESₙ, VSₙ = testDualPivotQuickSort(m, i) # Zamienione oznaczenie Cₙ i Sₙ (w tym miejscu to są tablice)
+    push!(SₙResults, Sₙ)
+    push!(ESₙResults, ESₙ)
+    push!(VSₙResults, VSₙ)
+    push!(Bounds1, ESₙ + 2*sqrt(VSₙ))
+    push!(Bounds2, ESₙ - 2*sqrt(VSₙ))
+    # EC = ((19/10)*(i + 1)*Harmonic(i+1)) - ((711/200)*(i+1)) + (3/2)
+    # push!(ECₙResults, EC)
+  end
+  ax[:plot](x, SₙResults, color="red", "+")
+  ax[:plot](x, ESₙResults, label="Dual Pivot QuickSort", color="red", linestyle="-")
+  ax[:plot](x, Bounds1, color="black", "--")
+  ax[:plot](x, Bounds2, color="black", "--")
+  ax[:legend](loc="best")
+  grid("on")
+  title("Swaps")
+end #plotDualPivotQuickSort
+
+fig, ax = PyPlot.subplots()
+plotComparisonsQuickSort(100, 10000, ax)
+plotComparisonsDualPivotQuickSort(100, 10000, ax)
+
+fig, ax = PyPlot.subplots()
+plotSwapsQuickSort(100, 10000, ax)
+plotSwapsDualPivotQuickSort(100, 10000, ax)
---
+++ Metody Probabilistyczne Algorytmiki/Lista_2/BallsInUrns.jl
@@ -0,0 +1,141 @@
+using PyPlot
+
+# m - liczba kul, n - liczba urn
+function BallsInUrns(m :: Int, n :: Int)
+    urns = zeros(Int64, n)
+    for i in 1:m
+        index = rand(1:n)
+        urns[index] += 1
+    end
+    return urns
+end # BallsInUrns
+
+# e - liczba eksperymentów, m powinno być równe n
+function MaxLoad(m :: Int, n :: Int, e :: Int)
+    results = []
+    for i in 1:e
+        push!(results, maximum(BallsInUrns(m, n)))
+    end
+    return results
+end # MaxAmountOfBalls
+
+# e - liczba eksperymentów, m powinno być równe n
+function EmptyUrns(m :: Int, n :: Int, e :: Int)
+    results = []
+    for i in 1:e
+        A = BallsInUrns(m, n)
+        empty = 0
+        for j in 1:n
+            if A[j] == 0
+                empty += 1
+            end
+        end
+        push!(results, empty)
+    end
+    return results
+end # EmptyUrns
+
+# m - liczba kul, n - liczba urn, e - liczba experymentów
+function PlotMaxLoad(m, n, e, ax)
+  x = []
+  results = []
+  expectedValue = []
+  experimentalExpectedValue = []
+  bounds = []
+  bounds2 = []
+  for i in 1000:1000:n
+    percentages = zeros(20)
+    push!(x, i)
+    maxLoad = MaxLoad(i, i, e)
+    push!(results, maxLoad)
+    E = 0
+    V² = 0
+    for j in 1:length(maxLoad)
+        E += maxLoad[j]
+        percentages[maxLoad[j]] += 1
+    end
+    E = E / length(maxLoad)
+    for j in 1:length(maxLoad)
+        V² += (maxLoad[j] - E)^2
+    end
+    V² = V² / length(maxLoad)
+    push!(experimentalExpectedValue, E)
+    push!(expectedValue, ((1 + 0.57)*log(i)) / (log(log(i))))
+    push!(bounds, [E + 2*sqrt(V²), E - 2*sqrt(V²)])
+    push!(bounds2, [log(i) / (log(log(i))), (3*log(i)) / (log(log(i)))])
+    len = length(maxLoad)
+    println("Liczba urn: ", i)
+    outsideBoundsCounter = 0
+    for k in 1:length(percentages)
+        if percentages[k] != 0
+            if k < E - 2*sqrt(V²) || k > E + 2*sqrt(V²)
+                outsideBoundsCounter += percentages[k]
+            end
+            println(k, " - ", percentages[k] / len)
+        end
+    end
+    println("Dane poza ograniczeniami: ", outsideBoundsCounter/len)
+  end
+  ax[:plot](x, results, color="red", "o")
+  ax[:plot](x, expectedValue, label="Teoretyczna wart. oczekiwana", color="blue", "-")
+  ax[:plot](x, experimentalExpectedValue, label="Wyznaczona eksperymentalnie wart. oczekiwana", color="black", "-")
+  ax[:plot](x, bounds, label="Ograniczenia z nier. Czebyszewa", color="red", "--")
+  ax[:plot](x, bounds2, color="green", "--")
+  ax[:legend](loc="best")
+
+  grid("on")
+  xlabel("Liczba urn")
+  ylabel("Max load")
+  title("Max Load")
+end # PlotMaxAmountOfBalls
+
+# m - liczba kul, n - liczba urn, e - liczba experymentów
+function PlotEmptyUrns(m, n, e, ax)
+  x = []
+  results = []
+  expectedValue = []
+  experimentalExpectedValue = []
+  bounds = []
+  for i in 1000:1000:n
+    push!(x, i)
+    emptyUrns = EmptyUrns(m, i, e)
+    push!(results, emptyUrns)
+    E = 0
+    V² = 0
+    for j in 1:length(emptyUrns)
+        E += emptyUrns[j]
+    end
+    E = E / length(emptyUrns)
+    for j in 1:length(emptyUrns)
+        V² += (emptyUrns[j] - E)^2
+    end
+    V² = V² / length(emptyUrns)
+    push!(experimentalExpectedValue, E)
+    push!(expectedValue, i*(1 - (1/i))^m)
+    push!(bounds, [E + 2*sqrt(V²), E - 2*sqrt(V²)])
+  end
+  ax[:plot](x, results, color="red", "+")
+  ax[:plot](x, expectedValue, label="Teoretyczna wart. oczekiwana", color="blue", "-")
+  ax[:plot](x, experimentalExpectedValue, label="Wyznaczona eksperymentalnie wart. oczekiwana", color="black", "-")
+  ax[:plot](x, bounds, label="Ograniczenia z nier. Czebyszewa", color="red", "--")
+  grid("on")
+  xlabel("Liczba urn")
+  ylabel("Liczba pustych urn")
+  annotate(string("Liczba kul: ", m),
+	xy=[1;0],
+	xycoords="axes fraction",
+	xytext=[-10,10],
+	textcoords="offset points",
+	fontsize=15.0,
+	ha="right",
+	va="bottom")
+  ax[:legend](loc="best")
+  title("Liczba pustych urn")
+end # PlotEmptyUrns
+
+fig, ax = PyPlot.subplots()
+PlotMaxLoad(10000, 10000, 1000, ax)
+# fig, ax = PyPlot.subplots()
+# PlotEmptyUrns(2000, 10000, 1000, ax)
+# PlotEmptyUrns(5000, 10000, 1000, ax)
+# PlotEmptyUrns(20000, 10000, 1000, ax)
