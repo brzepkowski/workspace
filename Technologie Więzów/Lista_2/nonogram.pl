e1dad5fa79f8358cd4f3a9959f207498e610f631
gothic17 <gothic1711@op.pl>
2017-12-12 16:02:01 +0100

    Poprawione szeregowanie zadań (Technologie Więzów), zrobione odpowiednie początki, trzeba dodać żeby zadania się nie pokrywały.

--- Technologie Więzów/Lista_2/nonogram.pl
+++ Technologie Więzów/Lista_2/nonogram.pl
@@ -26,10 +26,10 @@ generate_lists_for_blocks([_|Vs], N, [L|Ls]) :-
   L ins 0..1,
   generate_lists_for_blocks(Vs, N, Ls).
 
-generate_time_horizon(1, [0]).
-generate_time_horizon(T, [L|Ls]) :-
-  L is T - 1,
-  generate_time_horizon(L, Ls).
+generate_time_horizon(1, [1]).
+generate_time_horizon(T, [T|Ls]) :-
+  T1 is T - 1,
+  generate_time_horizon(T1, Ls).
 
 % L - lista zmiennych decyzyjnych
 only_one_beginning(L) :-
@@ -40,7 +40,8 @@ only_one_beginning(L) :-
 % [B|Bs] - tablica, w której będą zapisane wartości Xᵢ* Tᵢ
 inner_proper_beginning([], [], []).
 inner_proper_beginning([X|Xs], [T|Ts], [B|Bs]) :-
-  X*T #= B,
+  T1 is T - 1,
+  X*T1 #= B,
   inner_proper_beginning(Xs, Ts, Bs).
 
 % X - lista zmiennych decyzyjnych
@@ -48,7 +49,7 @@ inner_proper_beginning([X|Xs], [T|Ts], [B|Bs]) :-
 % B - moment gotowości
 proper_beginning(X, T, B) :-
   inner_proper_beginning(X, T, Bs),
-  sum(Bs, #>, B).
+  sum(Bs, #>=, B).
 
 % [B|Bs] - lista z długościami bloków
 % [L|Ls] - lista lsit przypisanych do każdego bloku
@@ -59,9 +60,9 @@ inner_forbid_overlapping([B|Bs], T, [L|Ls], [V|Vs]) :-
   T1 is T - B + 1,
   max(1, T1, Beginning),
   Len1 is T - Beginning + 1,
-  write("T: "), write(T), nl,
-  write("B: "), write(B), nl,
-  write("Len1: "), write(Len1), nl,
+  % write("T: "), write(T), nl,
+  % write("B: "), write(B), nl,
+  % write("Len1: "), write(Len1), nl,
   max(Len1, 0, Length),
   sublist(L, Beginning, Length, V),
   % write("V: "), write(V), nl,
@@ -80,18 +81,20 @@ forbid_overlapping(Blocks, [T|Ts], BlocksLists) :-
 % RowBlocks - tablica z długościami bloków
 % RowVars - zmienne decyzyjne z tego wiersza
 % N - długość wiersza
-add_row_constraints(RowBlocks, RowVars, N) :-
+add_row_constraints(RowBlocks, BlocksLists, N) :-
   generate_lists_for_blocks(RowBlocks, N, BlocksLists),
   generate_beginnings(RowBlocks, 0, Beginnings),
-  generate_time_horizon(N, TimeHorizon),
-  constrain_row_block(BlocksLists, TimeHorizon, Beginnings),
-  forbid_overlapping(RowBlocks, TimeHorizon, BlocksLists),
-  % write("BlocksLists: "), write(BlocksLists), nl,
-  flatten(BlocksLists, FlatBlocksLists),
-  label(FlatBlocksLists),
+  generate_time_horizon(N, TimeHorizonReversed),
+  reverse(TimeHorizonReversed, TimeHorizon),
+  write("TimeHorizon: "), write(TimeHorizon), nl,
   write("Beginnings: "), write(Beginnings), nl,
-  write("RowBlocks: "), write(RowBlocks), nl,
-  write("BlockLists: "), write(BlocksLists), nl.
+  constrain_row_block(BlocksLists, TimeHorizon, Beginnings),
+  % forbid_overlapping(RowBlocks, TimeHorizon, BlocksLists),
+  flatten(BlocksLists, FlatBlocksLists),
+  label(FlatBlocksLists).
+  % write("Beginnings: "), write(Beginnings), nl,
+  % write("RowBlocks: "), write(RowBlocks), nl,
+  % write("BlockLists: "), write(BlocksLists), nl.
 
 % [L|Ls] - lista list przypisana do bloków (zadań)
 % TimeHorizon - horyzont czasowy
@@ -99,13 +102,15 @@ add_row_constraints(RowBlocks, RowVars, N) :-
 constrain_row_block([], _, []).
 constrain_row_block([L|Ls], TimeHorizon, [B|Bs]) :-
   only_one_beginning(L),
+  % write("L: "), write(L), nl,
+  % write("B: "), write(B), nl,
   proper_beginning(L, TimeHorizon, B),
   constrain_row_block(Ls, TimeHorizon, Bs).
 
 % M - liczba wierszy, N - liczba kolumn
 nonogram(Vars, M, N, Row) :-
-  MN is M*N,
-  length(Vars, MN),
-  Vars ins 0..1,
-  add_row_constraints(Row, Vars, N),
-  label(Vars).
+  % MN is M*N,
+  % length(Vars, MN),
+  % Vars ins 0..1,
+  add_row_constraints(Row, Vars, N).
+  % label(Vars).
