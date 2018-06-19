b58cd28169a8d9ea9badd03a5c2fe187d32adfec
brzepkowski <bartosz.rzepkowski94@gmail.com>
2018-06-19 23:18:52 +0200

    Working modulard addition (using zeroed ancilla qubit and not using second CARRY to remove data from garbage - next problem to fix)

--- Masters Thesis/Shor.py
+++ Masters Thesis/Shor.py
@@ -312,19 +312,21 @@ def multiply_controlled_ADD_gate(circuit, qr, m, n, starting_index, a, controls)
         multiply_controlled_ADD_gate(circuit, qr, m, low_bits_length, starting_index, a_low_int, controls)
         multiply_controlled_ADD_gate(circuit, qr, m, high_bits_length, starting_index + low_bits_length, a_high_int, controls) #<-
 
+# This function is going to be launched only once (on the top level of the recursion)
 def multiply_controlled_ADD_gate_2(circuit, qr, m, n, starting_index, a, controls):
     print("ADD - n: ", n, ", a: ", a)
     if a > ((2**n) - 1):
         raise Exception("Constant which is supposed to be ADDed cannot be written using so few bits")
-    if n == 1:
-        if a == 1:
-            C_n_controlled_NOT_gate(circuit, qr, m, controls, starting_index)
+    if a >= 0:
+        a_binary = get_binary(a, n)
     else:
-        if a >= 0:
-            a_binary = get_binary(a, n)
-        else:
-            a_binary = get_twos_complement_binary(-a, n)
-        print("a_binary: ", a_binary)
+        a_binary = get_twos_complement_binary(-a, n)
+    print("a_binary: ", a_binary)
+    if n == 3:
+        a_2 =  int(a_binary[1:], 2)
+        print("a2: ", a_2)
+        multiply_controlled_ADD_gate(circuit, qr, m, 2, starting_index, a_2, controls)
+    else:
         low_bits_length = math.ceil(n/2)
         high_bits_length = math.floor(n/2)
         a_low_bits = a_binary[high_bits_length:n] # Numbers stored here and below are in reverse order (binarly)
@@ -388,8 +390,8 @@ def multiply_controlled_ADD_MOD_gate(circuit, qr, m, n, starting_index, a, N, co
 # WARNING: this function can be applied only to EVEN n and also n/2 must be even (it is caused by the contruction
 # of the CARRY gate, which needs EVEN number of ancilla and target qubits)
 def shor(circuit, qr, cr, m, n, a):
-    a = 3
-    N = 4
+    a = 6
+    N = 9
     NOT_gate(circuit, qr, m, 0)
     NOT_gate(circuit, qr, m, 1)
     # NOT_gate(circuit, qr, m, 2)
@@ -408,6 +410,7 @@ def shor(circuit, qr, cr, m, n, a):
     # multiply_controlled_CARRY_gate(circuit, qr, m, 4, 0, 3, [5,6,7])
     # multiply_controlled_ADD_gate_2(circuit, qr, m, 4, 0, -2, [])
     multiply_controlled_ADD_MOD_gate(circuit, qr, m, 10, 0, a, N, [])
+    # multiply_controlled_ADD_gate_2(circuit, qr, m, 5, 0, 2, [])
     # -------------Barrier before measurement------------
     circuit.barrier(qr)
     # measure
